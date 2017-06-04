d8b2da79fc1e78e9090ba9def8853b0a42d01ebd
timothytong <timothy.tong@hotmail.ca>
2017-06-03 22:26:19 -0400

    Add retrofit and gson library

---
+++ app/src/main/java/com/example/peterchu/watplanner/Constants.java
@@ -0,0 +1,9 @@
+package com.example.peterchu.watplanner;
+
+/**
+ * Created by Timothy Tong on 6/3/17.
+ */
+
+public class Constants {
+    public static final String API_KEY = "be6446c25133e4dba8cb087dbf9033ff";
+}
